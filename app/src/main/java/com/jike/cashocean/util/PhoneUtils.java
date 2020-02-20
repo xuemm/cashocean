defa871af47a8028595efe29bb7f943a2caefc4c
薛明明 <15330296957@163.com>
2020-02-20 10:07:32 +0800

    1.0.6

---
+++ app/src/main/java/com/jike/cashocean/util/PhoneUtils.java
@@ -0,0 +1,44 @@
+package com.jike.cashocean.util;
+
+import android.os.Build;
+
+import java.util.UUID;
+
+/**
+ * @author Ming
+ * @Date on 2020/1/22
+ * @Description 不使用权限生成一个Android设备的唯一标识码
+ * https://blog.csdn.net/a360940265a/article/details/79907844
+ */
+public class PhoneUtils {
+    //获得独一无二的Psuedo ID
+    public static String getUniquePsuedoID() {
+        String serial = null;
+
+        String m_szDevIDShort = "35" +
+                Build.BOARD.length() % 10 + Build.BRAND.length() % 10 +
+
+                Build.CPU_ABI.length() % 10 + Build.DEVICE.length() % 10 +
+
+                Build.DISPLAY.length() % 10 + Build.HOST.length() % 10 +
+
+                Build.ID.length() % 10 + Build.MANUFACTURER.length() % 10 +
+
+                Build.MODEL.length() % 10 + Build.PRODUCT.length() % 10 +
+
+                Build.TAGS.length() % 10 + Build.TYPE.length() % 10 +
+
+                Build.USER.length() % 10; //13 位
+
+        try {
+            serial = android.os.Build.class.getField("SERIAL").get(null).toString();
+            //API>=9 使用serial号
+            return new UUID(m_szDevIDShort.hashCode(), serial.hashCode()).toString();
+        } catch (Exception exception) {
+            //serial需要一个初始化
+            serial = "phone_imei"; // 随便一个初始化
+        }
+        //使用硬件信息拼凑出来的15位号码
+        return new UUID(m_szDevIDShort.hashCode(), serial.hashCode()).toString();
+    }
+}
